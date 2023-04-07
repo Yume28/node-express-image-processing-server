44b941ef3621ce2a36cecb9daf41c877c9f7c64a
Yume28 <steevensept09@gmail.com>
2023-04-07 13:04:21 +0800

    ...

---
+++ api/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { request } = require('http');
+const path = require('path');
+
+const app = express();
+
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
+app.use('/*', (request, response) => {
+   response.sendFile(pathToIndex); 
+});
+module.exports = apps;
