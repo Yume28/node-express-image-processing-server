51eea55221263705b408ea37b5550f87858e7f1b
Yume28 <steevensept09@gmail.com>
2023-04-07 13:38:31 +0800

    changes made to app

---
+++ api/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const { request } = require('http');
+const path = require('path');
+
+const app = express();
+
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
+app.use('/*', (request, response) => {
+   response.sendFile(pathToIndex); 
+});
+module.exports = apps;
