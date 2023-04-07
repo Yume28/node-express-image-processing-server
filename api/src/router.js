b92024d3c1d444f9fd14fa9591b12ee8e3f54e95
Yume28 <steevensept09@gmail.com>
2023-04-07 15:55:54 +0800

    ...

--- api/src/router.js
+++ api/src/router.js
@@ -24,12 +24,12 @@ const fileFilter = (request, file, callback) => {
 const upload = multer ({
     fileFilter,
     storage,
-})
+});
 
 router.post('/upload', upload.single('photo'), (request, response) =>{
     if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
 
     return response.status(201).json({sucess: true});
-})
+});
 
 module.exports = router;
